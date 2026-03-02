91d09335987c18bb76ae1096741e47e088760487
CJOrGo <L24270014@tuxtla.tecnm.mx>
2026-03-02 03:14:41 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/estructuras-control/', // Reemplaza con el nombre exacto de tu repositorio
-  plugins: [vue()],
-})
